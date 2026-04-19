ead57a7d5e80ca5bc46cc39050aedd264b0f83ec
Meghana Ramavath <ramavathmeghana08@gmail.com>
2026-04-19 18:24:52 +0530

    database change

--- server.ts
+++ server.ts
@@ -13,13 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // ---------------- MYSQL CONNECTION ----------------
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
