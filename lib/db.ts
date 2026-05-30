b90e4024b5296c2312eaa58f139f6922151866ca
Meghana Ramavath <ramavathmeghana08@gmail.com>
2026-05-30 09:17:53 +0530

    SBC2

--- lib/db.ts
+++ lib/db.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://slhnvmceuuubjkptkrid.supabase.co",
-  "sb_publishable_Hi4Ga9KNKgFIU1NsNXIoAA_o5fjPDdP"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsaG52bWNldXV1YmprcHRrcmlkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDA0OTM3MywiZXhwIjoyMDk1NjI1MzczfQ.1uioyH8Vxx8dbRRJU1J28pnFPJXHX6aszcmLiNMEhZ4"
 );
