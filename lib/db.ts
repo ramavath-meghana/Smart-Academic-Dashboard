c7d7fd21bd8a2d8aaf562956c1ae7ae272de2ff2
Meghana Ramavath <ramavathmeghana08@gmail.com>
2026-05-30 09:03:15 +0530

    SBC1

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase environment variables");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  "https://slhnvmceuuubjkptkrid.supabase.co",
+  "sb_publishable_Hi4Ga9KNKgFIU1NsNXIoAA_o5fjPDdP"
+);
